096dcf3cf9d985150289288815df51dfa6492edf
Mabel <mabelaldanafp@gmail.com>
2023-04-20 00:13:14 +0200

    parte 2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,3 +2,9 @@ package NuevoProyecto;
 
 import java.util.Scanner;
 public class P14 {
+	 public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        System.out.println("Introduce un numero");
+	        int numero=sc.nextInt();
+	        if (numero%2==0){
+}
