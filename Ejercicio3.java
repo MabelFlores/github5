e8dc7dc38b98146f65a4859d4abc15b279c3c878
Mabel <mabelaldanafp@gmail.com>
2023-04-20 00:19:53 +0200

    parte 2 ej 3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -4,3 +4,11 @@ import java.util.Scanner;
 public class P16 {
 	 public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Introduce un dia de la semana");
+	        String dia = sc.next();
+	        switch(dia){
+         case "lunes":
+         case "martes":
+         case "miercoles":
+         case "jueves":
+         case "viernes":
