b46610ceb7781b07e4103679d632d88919ac20c2
Mabel <mabelaldanafp@gmail.com>
2023-04-20 00:16:35 +0200

    parte 2 ej 2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,3 +1,8 @@
 package NuevoProyecto;
 import java.util.Scanner;
 public class P15 {
+	public static void main(String[] args) {
+		 int codigo;
+	        Scanner sc = new Scanner(System.in);
+	        do{
+}
